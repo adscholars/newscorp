24678d26162beca12a21c6a47adbc21f723f873d
Kavinkumar Subramani <207867737+adscholars@users.noreply.github.com>
2026-07-15 04:46:20 +0530

    Updated the Uniti previews.
    Updated the Uniti previews.

--- unitiinternet/settings.js
+++ unitiinternet/settings.js
@@ -3,47 +3,47 @@ campaignName: "Uniti Internet | NEWS-0000010 | August 2026 Display",
 brandColour: "#000000, #d4ae38",
 banners: [
     {
-        creativeName: "Set_1",
+        creativeName: "Set_1_500_50",
         route: "",
         sizes: [
-            { size: "160x600", path: "UnitiInternet_August_2026_Display_Set_1_160x600" },
-            { size: "300x600", path: "UnitiInternet_August_2026_Display_Set_1_300x600" },
-            { size: "300x250", path: "UnitiInternet_August_2026_Display_Set_1_300x250" },
-            { size: "320x50", path: "UnitiInternet_August_2026_Display_Set_1_320x50" },
-            { size: "728x90", path: "UnitiInternet_August_2026_Display_Set_1_728x90" },
+            { size: "160x600", path: "UnitiInternet_August_2026_Display_Set_1_500_50_160x600" },
+            { size: "300x600", path: "UnitiInternet_August_2026_Display_Set_1_500_50_300x600" },
+            { size: "300x250", path: "UnitiInternet_August_2026_Display_Set_1_500_50_300x250" },
+            { size: "320x50", path: "UnitiInternet_August_2026_Display_Set_1_500_50_320x50" },
+            { size: "728x90", path: "UnitiInternet_August_2026_Display_Set_1_500_50_728x90" },
         ]
     },
     {
-        creativeName: "Set_2",
+        creativeName: "Set_2_1000_100",
         route: "",
         sizes: [
-            { size: "160x600", path: "UnitiInternet_August_2026_Display_Set_2_160x600" },
-            { size: "300x600", path: "UnitiInternet_August_2026_Display_Set_2_300x600" },
-            { size: "300x250", path: "UnitiInternet_August_2026_Display_Set_2_300x250" },
-            { size: "320x50", path: "UnitiInternet_August_2026_Display_Set_2_320x50" },
-            { size: "728x90", path: "UnitiInternet_August_2026_Display_Set_2_728x90" },
+            { size: "160x600", path: "UnitiInternet_August_2026_Display_Set_2_1000_100_160x600" },
+            { size: "300x600", path: "UnitiInternet_August_2026_Display_Set_2_1000_100_300x600" },
+            { size: "300x250", path: "UnitiInternet_August_2026_Display_Set_2_1000_100_300x250" },
+            { size: "320x50", path: "UnitiInternet_August_2026_Display_Set_2_1000_100_320x50" },
+            { size: "728x90", path: "UnitiInternet_August_2026_Display_Set_2_1000_100_728x90" },
         ]
     },
     {
-        creativeName: "Set_3",
+        creativeName: "Set_3_24M_Contract",
         route: "",
         sizes: [
-            { size: "160x600", path: "UnitiInternet_August_2026_Display_Set_3_160x600" },
-            { size: "300x600", path: "UnitiInternet_August_2026_Display_Set_3_300x600" },
-            { size: "300x250", path: "UnitiInternet_August_2026_Display_Set_3_300x250" },
-            { size: "320x50", path: "UnitiInternet_August_2026_Display_Set_3_320x50" },
-            { size: "728x90", path: "UnitiInternet_August_2026_Display_Set_3_728x90" },
+            { size: "160x600", path: "UnitiInternet_August_2026_Display_Set_3_24M_Contract_160x600" },
+            { size: "300x600", path: "UnitiInternet_August_2026_Display_Set_3_24M_Contract_300x600" },
+            { size: "300x250", path: "UnitiInternet_August_2026_Display_Set_3_24M_Contract_300x250" },
+            { size: "320x50", path: "UnitiInternet_August_2026_Display_Set_3_24M_Contract_320x50" },
+            { size: "728x90", path: "UnitiInternet_August_2026_Display_Set_3_24M_Contract_728x90" },
         ]
     },
     {
-        creativeName: "Set_4",
+        creativeName: "Set_4_Generic",
         route: "",
         sizes: [
-            { size: "160x600", path: "UnitiInternet_August_2026_Display_Set_4_160x600" },
-            { size: "300x600", path: "UnitiInternet_August_2026_Display_Set_4_300x600" },
-            { size: "300x250", path: "UnitiInternet_August_2026_Display_Set_4_300x250" },
-            { size: "320x50", path: "UnitiInternet_August_2026_Display_Set_4_320x50" },
-            { size: "728x90", path: "UnitiInternet_August_2026_Display_Set_4_728x90" },
+            { size: "160x600", path: "UnitiInternet_August_2026_Display_Set_4_Generic_160x600" },
+            { size: "300x600", path: "UnitiInternet_August_2026_Display_Set_4_Generic_300x600" },
+            { size: "300x250", path: "UnitiInternet_August_2026_Display_Set_4_Generic_300x250" },
+            { size: "320x50", path: "UnitiInternet_August_2026_Display_Set_4_Generic_320x50" },
+            { size: "728x90", path: "UnitiInternet_August_2026_Display_Set_4_Generic_728x90" },
         ]
     }
 ]};
