69b2315a4fe30164a37adb27a8463fd1c7d0c094
Kavinkumar Subramani <207867737+adscholars@users.noreply.github.com>
2026-07-15 10:38:35 +0530

    Updated the preview.
    Updated the preview.

---
+++ unitiinternet/banners/Set_1_500_50/UnitiInternet_August_2026_Display_Set_1_500_50_300x600/js/logic.js
@@ -0,0 +1,50 @@
+function bannerAnimation(){
+    var tl = new TimelineMax({ repeat: 1, repeatDelay: 3, });
+
+    TweenMax.set([ copy1, logoBg, bottomshape,  optlogo, logo],  {opacity: 1, });
+    TweenMax.set([cta ],  { opacity: 1, scale: 0, transformOrigin: "80px 560px" });
+    TweenMax.set([product],  {x: 200, opacity: 1 });
+    TweenMax.set([offer ],  { opacity: 1, scale: 0, transformOrigin: "210px 350px" });
+    TweenMax.set([copy1, price1, price2],  {x: -250, opacity: 1 });
+
+
+    // TweenMax.to(copy1, 0.5, { x:0, opacity: 1, ease: Quad.easeOut, delay: 0 });
+    // TweenMax.to(price1, 0.5, { x:0, opacity: 1, ease: Quad.easeOut, delay: 0.5 });
+    // TweenMax.to(price2, 0.5, { x:0, opacity: 1, ease: Quad.easeOut, delay: 0.75 });
+    // TweenMax.to(product, 0.5, { x:0, opacity: 1, ease: Quad.easeOut, delay: 1 });
+    
+    // TweenMax.to([offer], 0.65, { scale: 1, ease: Back.easeOut, delay: 1.5 });
+    // TweenMax.to([cta], 0.65, { scale: 1, ease: Back.easeOut, delay: 2.25 });
+
+
+
+     tl.to(copy1, 0.5, { x: 0, ease: Quad.easeOut })
+      .to(price1, 0.5, { x: 0, ease: Quad.easeOut })
+      .to(price2, 0.5, { x: 0, ease: Quad.easeOut },)
+      .to(product, 0.5, { x: 0, ease: Quad.easeOut })
+      .to(offer, 0.65, { scale: 1, ease: Back.easeOut })
+      .to(cta, 0.65, { scale: 1, ease: Back.easeOut });
+}
+
+function exitbanner() {
+    window.open(clickTag, "_blank");
+}
+
+function mouseOvers(){
+    TweenMax.to(cta, 0.35, { scale: 1.15, ease: Quad.easeOut, delay: 0 });
+    TweenMax.to(cta, 0.5, { scale: 1, ease: Quad.easeOut, delay: 0.25 });
+
+}
+function mouseleaves(){
+    TweenMax.to(cta, 0.5, { scale: 1.15, ease: Quad.easeOut, delay: 0 });
+    TweenMax.to(cta, 0.35, { scale: 1, ease: Quad.easeOut,  delay: 0.25 });
+}
+
+function startBanner(){
+    var clickelement = document.getElementById("banner");
+    clickelement.addEventListener("click", exitbanner);
+    bannerAnimation();
+}
+
+
+window.addEventListener("load", startBanner);
---
+++ unitiinternet/settings.js
@@ -0,0 +1,49 @@
+const PREVIEW_SETTINGS = {
+campaignName: "Uniti Internet | NEWS-0000010 | August 2026 Display",
+brandColour: "#000000, #d4ae38",
+banners: [
+    {
+        creativeName: "Set_1_500_50",
+        route: "",
+        sizes: [
+            { size: "160x600", path: "UnitiInternet_August_2026_Display_Set_1_500_50_160x600" },
+            { size: "300x600", path: "UnitiInternet_August_2026_Display_Set_1_500_50_300x600" },
+            { size: "300x250", path: "UnitiInternet_August_2026_Display_Set_1_500_50_300x250" },
+            { size: "320x50", path: "UnitiInternet_August_2026_Display_Set_1_500_50_320x50" },
+            { size: "728x90", path: "UnitiInternet_August_2026_Display_Set_1_500_50_728x90" },
+        ]
+    },
+    {
+        creativeName: "Set_2_1000_100",
+        route: "",
+        sizes: [
+            { size: "160x600", path: "UnitiInternet_August_2026_Display_Set_2_1000_100_160x600" },
+            { size: "300x600", path: "UnitiInternet_August_2026_Display_Set_2_1000_100_300x600" },
+            { size: "300x250", path: "UnitiInternet_August_2026_Display_Set_2_1000_100_300x250" },
+            { size: "320x50", path: "UnitiInternet_August_2026_Display_Set_2_1000_100_320x50" },
+            { size: "728x90", path: "UnitiInternet_August_2026_Display_Set_2_1000_100_728x90" },
+        ]
+    },
+    {
+        creativeName: "Set_3_24M_Contract",
+        route: "",
+        sizes: [
+            { size: "160x600", path: "UnitiInternet_August_2026_Display_Set_3_24M_Contract_160x600" },
+            { size: "300x600", path: "UnitiInternet_August_2026_Display_Set_3_24M_Contract_300x600" },
+            { size: "300x250", path: "UnitiInternet_August_2026_Display_Set_3_24M_Contract_300x250" },
+            { size: "320x50", path: "UnitiInternet_August_2026_Display_Set_3_24M_Contract_320x50" },
+            { size: "728x90", path: "UnitiInternet_August_2026_Display_Set_3_24M_Contract_728x90" },
+        ]
+    },
+    {
+        creativeName: "Set_4_Generic",
+        route: "",
+        sizes: [
+            { size: "160x600", path: "UnitiInternet_August_2026_Display_Set_4_Generic_160x600" },
+            { size: "300x600", path: "UnitiInternet_August_2026_Display_Set_4_Generic_300x600" },
+            { size: "300x250", path: "UnitiInternet_August_2026_Display_Set_4_Generic_300x250" },
+            { size: "320x50", path: "UnitiInternet_August_2026_Display_Set_4_Generic_320x50" },
+            { size: "728x90", path: "UnitiInternet_August_2026_Display_Set_4_Generic_728x90" },
+        ]
+    }
+]};
